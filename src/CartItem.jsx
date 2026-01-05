d068b3d0e78d9fc89aebd58df763a380a9bfbac5
thorington26 <latrisha.ct@gmail.com>
2026-01-04 19:13:46 -0500

    initial commit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,23 +9,31 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach(item =>{
+        total += parseFloat(item.cost.substring(1)) * item.quantity ;
+    });
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem({ name: item.name, image: item.image, cost:item,cost}));
   };
 
   // Calculate total cost based on quantity for an item
